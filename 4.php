6463c52154f279a397b0f73885548105b9c9b6a1
DensetsuOne <densetsu2015@mail.ru>
2021-05-19 17:36:22 +0700

    finish

--- 4.php
+++ 4.php
@@ -1,6 +1,6 @@
 <?php
 $result = 0;
-$num = [1,1,2,3,3,3,4,4,5,5,5];
+$num = [1,1,2,3,3,3,4,4,-51,5,5,5,51,-5,-5,50,50,-51,-51];
 $i = 0;
 $s = $i +1;
 $num2 = $num[$s];
@@ -8,9 +8,10 @@ while($i <= count($num)){
     if($num[$s] == null){
         break;
     }else{
-    if(count($num)-1 >= $num2){
+    if(count($num)-1 >= $s){
         $flag = false;
     while($flag == false){
+        
     if($num[$i] == $num2){
         $result += 1;
         $i++;
